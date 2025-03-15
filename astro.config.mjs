ceab96d6cb30658c0db8aa8d8c450579e763807d
JDgomez2002 <87883400+JDgomez2002@users.noreply.github.com>
2025-03-14 18:21:37 -0600

    feat: layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+import react from "@astrojs/react";
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
+
   adapter: node({
-    mode: "standalone"
+    mode: "standalone",
   }),
-  output: "server"
+
+  output: "server",
+  integrations: [react()],
 });

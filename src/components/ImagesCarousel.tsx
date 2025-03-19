87296def7954c059df80724d48f3c76a8242db86
JDgomez2002 <87883400+JDgomez2002@users.noreply.github.com>
2025-03-19 12:43:42 -0600

    feat: school images

--- src/components/ImagesCarousel.tsx
+++ src/components/ImagesCarousel.tsx
@@ -8,15 +8,33 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel"
 
-import school_1 from "@/assets/school_1.webp"
-import school_2 from "@/assets/school_2.webp"
-import school_3 from "@/assets/school_3.webp"
+import school_1 from "@/assets/school_1.jpeg"
+import school_2 from "@/assets/school_2.jpeg"
+import school_3 from "@/assets/school_3.jpeg"
+import school_4 from "@/assets/school_4.jpeg"
+import school_5 from "@/assets/school_5.jpeg"
+import school_6 from "@/assets/school_6.jpeg"
+import school_7 from "@/assets/school_7.jpeg"
+import school_8 from "@/assets/school_8.jpeg"
+import school_9 from "@/assets/school_9.jpeg"
+import school_10 from "@/assets/school_10.jpeg"
 
 export default function ImagesCarousel() {
 
     const height = "h-[30rem]"
 
-    const images = [school_1, school_2, school_3]
+    const images = [
+        school_1,
+        school_2,
+        school_3,
+        school_4,
+        school_5,
+        school_6,
+        school_7,
+        school_8,
+        school_9,
+        school_10,
+    ]
 
     return (
         <Carousel

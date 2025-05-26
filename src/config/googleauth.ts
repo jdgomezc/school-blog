ff0eec31bb284b93100391ff165b2d8fa56bc0a5
JDgomez2002 <87883400+JDgomez2002@users.noreply.github.com>
2025-05-26 12:01:03 -0600

    feat: service account json in base 64

--- src/config/googleauth.ts
+++ src/config/googleauth.ts
@@ -1,19 +1,26 @@
-import { JWT } from 'google-auth-library'
+import { JWT } from "google-auth-library";
+import { GOOGLE_SERVICE_ACCOUNT } from "astro:env/client";
 
-const SCOPES = ['https://www.googleapis.com/auth/drive']
+const SCOPES = ["https://www.googleapis.com/auth/drive"];
 
 export async function authorize() {
+  const buffer = Buffer.from(GOOGLE_SERVICE_ACCOUNT, "base64");
+  const serviceAccount = JSON.parse(buffer.toString("utf-8"));
+
+  console.log("serviceAccount:", serviceAccount);
 
   try {
-    const authGoogleClient = new JWT({ // Use JWT directly
-      keyFile: "./service_account.json",
+    const authGoogleClient = new JWT({
+      // Use JWT directly
+      // keyFile: "./service_account.json",
+      key: serviceAccount,
       scopes: SCOPES,
     });
 
     await authGoogleClient.authorize(); // Authorize the client
     return authGoogleClient; // Return the JWT client
   } catch (error) {
-    console.error('Error authorizing with service account:', error)
-    throw error
+    console.error("Error authorizing with service account:", error);
+    throw error;
   }
 }
